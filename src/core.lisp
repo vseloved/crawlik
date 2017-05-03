358afa1070140ff6d01e6ba1a31e46854a824908
Vsevolod Dyomkin <vseloved@gmail.com>
2017-05-03 16:51:01 +0300

    forgotten udpate

--- src/core.lisp
+++ src/core.lisp
@@ -4,21 +4,21 @@
 (in-package #:crawlik)
 (named-readtables:in-readtable rutilsx-readtable)
 
-(defgeneric srape (site source)
+(defgeneric scrape (site source)
   (:documentation
    "Extract data from SOURCE according to the logic of a particular SITE.
     The methods of this function should return the data as first value
     and, optionally, the link to the next page to crawl as the second one."))
 
-(defgeneric crawl (site &optional url next-fn)
+(defgeneric crawl (site)
   (:documentation
-   ""))
+   "Process all the pages of the SITE according to some rule in "))
 
-(defmethod crawl (site &optional url next-fn)
-  (let ((raw (drakma:http-request url))
+(defmethod crawl (site)
+  (let ((raw (drakma:http-request @site.url))
         rez)
     (loop (with ((content next (scrape site raw)))
             (push content rez)
-            (when (or next next-fn)
-              (:= raw (drakma:http-request (or next (call next-fn)))))))
+            (when (or next @site.next-fn)
+              (:= raw (drakma:http-request (or next (call @site.next-fn)))))))
     (reverse rez)))
